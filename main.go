18216b39d232f1adbb09cfffc1b9cb774b56c94b
david.walshe93@gmail.com <david.walshe93@gmail.com>
2020-04-16 13:22:35 +0100

    Used the function syntax to create a new function with a return type.

--- main.go
+++ main.go
@@ -2,8 +2,14 @@ package main
 
 import "fmt"
 
+// Main Caller
 func main() {
 	card := "Ace of Spades"
 
 	fmt.Println(card)
+}
+
+// Returns a new card name
+func newCard() string {
+	return "Five of Diamonds"
 }
